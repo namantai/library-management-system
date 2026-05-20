1a7931c025cc3332f2e6aaef3c5a56adc337e5d8
Nurtas Amantay <mamaevnrken@gmail.com>
2026-05-20 14:56:19 +0500

    Implement JSON file handling

---
+++ services/file_service.py
@@ -0,0 +1,9 @@
+import json
+
+def load_data(filename):
+    with open(filename, "r") as file:
+        return json.load(file)
+
+def save_data(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
